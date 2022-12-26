d8aff1b5e30466ef4bdd5589174d5bf5ed63ebed
Bill Drol <billdrol64@gmail.com>
2022-12-26 17:33:33 -0500

    Cleanup

--- scripts/dark.js
+++ scripts/dark.js
@@ -1,14 +1,18 @@
 
-if ( localStorage.getItem( 'theme' ) == null )
-{
-    localStorage.setItem( 'theme' , 'dark' ) // Default theme.
-}
+( function() {
 
-if ( localStorage.getItem( 'theme' ) == 'dark' )
-{
-    document.documentElement.classList.add( 'dark' )
-}
-else
-{
-    document.documentElement.classList.remove( 'dark' )
-}
+    if ( localStorage.getItem( 'theme' ) == null )
+    {
+        localStorage.setItem( 'theme' , 'dark' ) // Default theme.
+    }
+
+    if ( localStorage.getItem( 'theme' ) == 'dark' )
+    {
+        document.documentElement.classList.add( 'dark' )
+    }
+    else
+    {
+        document.documentElement.classList.remove( 'dark' )
+    }
+
+} ) ()
--- scripts/website.js
+++ scripts/website.js
@@ -1,54 +1,58 @@
 
-function initTheme()
-{
-    document.querySelector( '.toggle-dark' ).addEventListener( 'click' , ( event ) =>
+( function() {
+
+    function initTheme()
     {
-        event.preventDefault()
-
-        if ( localStorage.getItem( 'theme' ) == 'dark' )
+        document.querySelector( '.toggle-dark' ).addEventListener( 'click' , ( event ) =>
         {
-            localStorage.setItem( 'theme' , '' )
+            event.preventDefault()
 
-            document.documentElement.classList.remove( 'dark' )
-        }
-        else
-        {
-            localStorage.setItem( 'theme' , 'dark' )
-
-            document.documentElement.classList.add( 'dark' )
-        }
-    })
-}
-
-
-function initWorkSamples()
-{
-    const intersection = new IntersectionObserver( ( items , observer ) =>
-    {
-        items.forEach( item =>
-        {
-            if ( item.isIntersecting )
+            if ( localStorage.getItem( 'theme' ) == 'dark' )
             {
-                item.target.classList.add( 'show' )
-                observer.unobserve( item.target )
+                localStorage.setItem( 'theme' , '' )
+
+                document.documentElement.classList.remove( 'dark' )
+            }
+            else
+            {
+                localStorage.setItem( 'theme' , 'dark' )
+
+                document.documentElement.classList.add( 'dark' )
             }
         })
-
-    } , { threshold :  0.33 /* When 30 percent of item is visible. */ } )
-
-    document.querySelectorAll( '.work-sample' ).forEach( sample => intersection.observe( sample ) )
-}
+    }
 
 
-function init()
-{
-    initTheme()
-    initWorkSamples()
-}
+    function initWorkSamples()
+    {
+        const intersection = new IntersectionObserver( ( items , observer ) =>
+        {
+            items.forEach( item =>
+            {
+                if ( item.isIntersecting )
+                {
+                    item.target.classList.add( 'show' )
+                    observer.unobserve( item.target )
+                }
+            })
+
+        } , { threshold :  0.33 /* When 30 percent of item is visible. */ } )
+
+        document.querySelectorAll( '.work-sample' ).forEach( sample => intersection.observe( sample ) )
+    }
 
 
-//----- When page loads.
-window.addEventListener( 'load' , function()
-{
-    setTimeout( init , 175 )
-})
+    function init()
+    {
+        initTheme()
+        initWorkSamples()
+    }
+
+
+    //----- When page loads.
+    window.addEventListener( 'load' , function()
+    {
+        setTimeout( init , 175 )
+    })
+
+} ) ()
